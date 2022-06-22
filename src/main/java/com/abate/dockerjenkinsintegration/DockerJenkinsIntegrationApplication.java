c0e005559573629f4003554ec7fa6a345d54ede4
abate.abtew <abate.abtew@gmail.com>
2022-06-22 15:00:12 -0400

    Second commit

--- src/main/java/com/abate/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/abate/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
@@ -2,8 +2,10 @@ package com.abate.dockerjenkinsintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerJenkinsIntegrationApplication {
 
 	public static void main(String[] args) {
